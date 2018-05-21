f10f4439657beb979bd57adfa14f2c2ec817c491
Unknown <hi1da.8.o0no4me@docomo.ne.jp>
2018-05-21 17:11:28 +0900

    ルーレット2回目

---
+++ Roulete/Assets/RouleteControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouleteControler : MonoBehaviour {
+
+    // Use this for initialization
+
+    float rotSpeed = 0;
+    int i;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 20f;
+            i = 0;
+        }
+        transform.Rotate(0, 0, rotSpeed);
+        if(i == 90)
+        {
+            this.rotSpeed *= 0.985f;
+        }
+        else
+        {
+            i++;
+        }
+	}
+}
